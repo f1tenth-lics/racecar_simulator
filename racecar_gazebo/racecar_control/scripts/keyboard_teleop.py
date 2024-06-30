5f84eb26e7f26319fbba360a201a3f48d175eb97
Joshua J. Damanik <joshuajdmk@gmail.com>
2024-06-30 22:07:11 +0900

    Update keyboard_teleop.py

--- racecar_gazebo/racecar_control/scripts/keyboard_teleop.py
+++ racecar_gazebo/racecar_control/scripts/keyboard_teleop.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 import sys
@@ -6,59 +6,72 @@ import select
 import termios
 import tty
 
-from ackermann_msgs.msg import AckermannDrive
+from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
+from std_msgs.msg import Header
 
-keyBindings = {'w':(1.0,  0.0),  # move forward
-               'd':(1.0, -1.0), # move foward and right
-               'a':(1.0 , 1.0),  # move forward and left
-               's':(-1.0, 0.0), # move reverse
-               'q':(0.0,  0.0)}  # all stop
+keyBindings = {'\x41': (1.0, 0.0),  # move forward (Up Arrow)
+               '\x43': (1.0, -1.0),  # move forward and right (Right Arrow)
+               '\x44': (1.0, 1.0),  # move forward and left (Left Arrow)
+               '\x42': (-1.0, 0.0),  # move reverse (Down Arrow)
+               'q': (0.0, 0.0)}  # all stop (q)
 
-speed_limit = 0.250
+speed_limit = 4.0
 angle_limit = 0.325
 
+
 def getKey():
-   tty.setraw(sys.stdin.fileno())
-   select.select([sys.stdin], [], [], 0)
-   key = sys.stdin.read(1)
-   termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-   return key
+    tty.setraw(sys.stdin.fileno())
+    key = None
+    while True:
+        select.select([sys.stdin], [], [], 0)
+        key = sys.stdin.read(1)
+        if key is not None:
+            break
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
 
 def vels(speed, turn):
-  return 'currently:\tspeed {}\tturn {}'.format(speed, turn)
+    return 'currently:\tspeed {}\tturn {}'.format(speed, turn)
 
-if __name__== '__main__':
-  settings    = termios.tcgetattr(sys.stdin)
-  command_pub = rospy.Publisher('/{}/vesc/high_level/ackermann_cmd_mux/input/nav_0'.format(str(sys.argv[1])), AckermannDrive, queue_size = 1)
-  rospy.init_node('keyboard_teleop', anonymous = True)
 
-  speed  = 0.0
-  angle  = 0.0
-  status = 0.0
+if __name__ == '__main__':
+    print('Use Arrow keys to move the car. Press q to stop the car.')
 
-  try:
-    while True:
-       key = getKey()
-       if key in keyBindings.keys():
-          speed = keyBindings[key][0]
-          angle = keyBindings[key][1]
-       else:
-          speed = 0.0
-          angle = 0.0
-          if (key == '\x03'):
-             break
-       command                = AckermannDrive();
-       command.speed          = speed * speed_limit
-       command.steering_angle = angle * angle_limit
-       command_pub.publish(command)
+    settings = termios.tcgetattr(sys.stdin)
+    command_pub = rospy.Publisher('/{}/vesc/low_level/ackermann_cmd_mux/input/teleop'.format(str(sys.argv[1])),
+                                  AckermannDriveStamped, queue_size=1)
+    rospy.init_node('keyboard_teleop', anonymous=True)
 
-  except:
-    print('raise exception: key binding error')
+    speed = 0.0
+    angle = 0.0
+    status = 0.0
 
-  finally:
-    command = AckermannDrive();
-    command.speed = speed * speed_limit
-    command.steering_angle = angle * angle_limit
-    command_pub.publish(command)
+    try:
+        while True:
+            key = getKey()
+            if key in keyBindings.keys():
+                speed = keyBindings[key][0]
+                angle = keyBindings[key][1]
+            else:
+                speed = 0.0
+                angle = 0.0
+                if (key == '\x03'):
+                    break
+            command = AckermannDriveStamped()
+            command.header = Header()
+            command.drive.speed = speed * speed_limit
+            command.drive.steering_angle = angle * angle_limit
+            command_pub.publish(command)
 
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    except:
+        print('raise exception: key binding error')
+
+    finally:
+        command = AckermannDriveStamped()
+        command.header = Header()
+        command.drive.speed = speed * speed_limit
+        command.drive.steering_angle = angle * angle_limit
+        command_pub.publish(command)
+
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
